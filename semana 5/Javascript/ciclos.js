aca7c9ec4a4a21d6a08833cb7cd2fd3854f9f623
Steev99 <ricardov2016.12@gmail.com>
2024-11-07 22:44:18 -0500

    semana6

---
+++ semana 5/Javascript/ciclos.js
@@ -0,0 +1,55 @@
+/*11 let contar,
+    max,
+    msg="";
+max = parseInt(prompt("Ingrese un numero:"));
+for(contar=0; contar <max; contar++){
+    msg+= contar +",";
+}
+msg += contar;
+alert(msg);
+
+
+12 let numero,
+    msg="";
+for(contar=10; contar >=1; contar--){
+    msg+= contar +",";
+}
+msg += contar;
+alert(msg);
+
+13 let numero;
+for(numero= 2; numero <=20; numero+=2){
+    document.write(numero + " ")
+}
+
+14 let numero;
+for (numero = 1; numero <=19; numero +=2){
+    document.write(numero + " ");
+}
+
+15 let a,
+    numero,
+    resultado;
+
+numero= parseInt(prompt("Escribe un numero para multiplicar"));
+for (a=1; a<=10; a++){
+    resultado = numero*a;
+    document.write("<br>"+numero + "x" + a +"="+ resultado)
+}
+
+16 let suma,
+    resultado;
+
+for(suma = 1; suma<=10; suma++){
+    resultado += suma; 
+    document.write("<br>"+suma);
+}*/
+
+let numero,
+    a,
+    b;
+a=0;
+b=1;
+numero = parseInt(prompt("Ingrese el numero para la serie de fibonachi"));
+
+document.write("")
---
+++ semana 6/js/suma.js
@@ -0,0 +1,14 @@
+/*function suma(){
+    let num1 = parseInt(prompt("Ingresa el primer numero"));
+    let num2 = parseint(prompt("Ingrese el segundo numero"));
+    let resultado = num1 + num2;
+    
+    alert("La suma de los dos numeros es:" + resultado);
+}*/
+
+function suma() {
+    let num1 = parseInt(prompt("Ingrese el primer número: "));
+    let num2 = parseInt(prompt("Ingrese el segundo número: "));
+    let resultado = num1 + num2;
+    
+    }alert("El resultado de la suma es: " + resultado);
