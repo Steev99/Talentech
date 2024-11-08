62ddb6d955affb106bbbb6265ff79f2dca994c7d
Steev99 <ricardov2016.12@gmail.com>
2024-11-07 23:52:36 -0500

    semana6

--- semana 6/js/suma.js
+++ semana 6/js/suma.js
@@ -1,14 +1,16 @@
-/*function suma(){
-    let num1 = parseInt(prompt("Ingresa el primer numero"));
-    let num2 = parseint(prompt("Ingrese el segundo numero"));
-    let resultado = num1 + num2;
-    
-    alert("La suma de los dos numeros es:" + resultado);
-}*/
+function displey(valor) {
+    document.getElementById('entrada').value += valor;
+}
 
-function suma() {
-    let num1 = parseInt(prompt("Ingrese el primer número: "));
-    let num2 = parseInt(prompt("Ingrese el segundo número: "));
-    let resultado = num1 + num2;
-    
-    }alert("El resultado de la suma es: " + resultado);
+function borrar() {
+    document.getElementById('entrada').value = '';
+}
+function igual() {
+    const entrada = document.getElementById("entrada");
+    try {
+        // evalúa la expresión matemática
+        entrada.value = eval(entrada.value);
+    } catch (error) {
+        entrada.value = "Error";
+    }
+}
